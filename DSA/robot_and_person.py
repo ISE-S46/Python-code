a8ea9208ab867b2be32d0fe52dc0507dcabfed02
ISE <ise.sns46@gmail.com>
2025-08-06 13:57:38 +0700

    Add code for classes

---
+++ DSA/robot_and_person.py
@@ -0,0 +1,33 @@
+class Robot:
+    def __init__(self, n, c, w):
+        self.name = n
+        self.color = c
+        self.weight = w
+
+    def introduce_self(self):
+        print(f"My name is {self.name}")
+        print(f"I like color {self.color}")
+        print(f"My weight is {self.weight}")
+
+
+class Person:
+    def __init__(self, n, p, i):
+        self.name = n
+        self.personality = p
+        self.isSitting = i
+
+    def sit_down(self):
+        print(f"My name is ${self.name}")
+
+
+r1 = Robot("Tom", "red", 30)
+r2 = Robot("Jerry", "blue", 40)
+
+p1 = Person("Alice", "aggressive", False)
+p2 = Person("Becky", "aggressive", True)
+
+p1.robot_owned = r2
+p2.robot_owned = r1
+
+p1.robot_owned.introduce_self()
+p2.robot_owned.introduce_self()
